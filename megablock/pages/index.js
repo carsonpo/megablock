aa536a12f887efbf1e948dbc99266b4aa81e0a4f
Carson Poole <carson@placepull.com>
2020-07-29 04:16:25 -0400

    ADDED line break in copy

--- megablock/pages/index.js
+++ megablock/pages/index.js
@@ -69,8 +69,9 @@ export default function Home() {
                 🤖
               </span>
               . Get started by signing in with Twitter—we won't ever post on
-              your behalf. Or use your account in any other way than to block
-              people you ask us to.
+              your behalf.
+              <br /> Or use your account in any other way than to block people
+              you ask us to.
             </p>
             {!session ? (
               // Manual redirection
